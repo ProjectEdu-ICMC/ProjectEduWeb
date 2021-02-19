de2edb8e12186f202887bc38cac0dab11820cff5
alexandre jr <alexgalocha.jr@gmail.com>
2021-02-19 17:31:44 -0300

    adding conditional fields to infoform

--- client/src/components/forms/InfoForm.js
+++ client/src/components/forms/InfoForm.js
@@ -11,11 +11,11 @@ import InfoModel from '../../actions/Info';
 function InfoForm(props) {
     const { mod, topic } = useParams();
 
-    const { register, handleSubmit, errors } = useForm();
+    const { register, watch, handleSubmit, errors } = useForm();
     const { reset, info } = props;
 
     //const [ dict, ] = useContext(LanguageContext);
-
+    const watchDataType = watch('datatype');
     const dispatch = useDispatch();
 
     const initData = useSelector(state => state.info.array?.[info]);
@@ -62,7 +62,7 @@ function InfoForm(props) {
 
         reset();
     };
-
+    
 
     return ( 
         <div className='w-full h-screen flex items-center justify-center bg-opacity-75 bg-black fixed z-20 top-0 left-0'>
@@ -72,7 +72,7 @@ function InfoForm(props) {
                 </span>
                 <label 
                     className='text-sm text-gray-500 ml-1 mt-2'
-                    htmlFor='name'>
+                    htmlFor='type'>
                     Tipo da Informação
                 </label>
                 <select
@@ -87,10 +87,127 @@ function InfoForm(props) {
                     <option value='principle'>Princípio</option>
                     <option value='procedure'>Procedimento</option>
                 </select>
-                { errors.name && <p className='text-red-700 text-sm px-1'> 
-                    { errors.name.message } 
+
+                { errors.type && <p className='text-red-700 text-sm px-1'> 
+                    { errors.type.message } 
                 </p> }
 
+                <label 
+                    className='text-sm text-gray-500 ml-1 mt-2'
+                    htmlFor='datatype'>
+                    Tipo do dado
+                </label>
+                <div 
+                    className='flex flex-wrap'
+                >
+                    <input 
+                        ref={ register({ required: 'Selecione um tipo de dados  para a Informação' }) }
+                        className='mr-1'
+                        type='radio' 
+                        name='datatype' 
+                        value='text' 
+                        id='text' />
+                    <label 
+                        className='text-gray-800'
+                        htmlFor='text'
+                    >
+                        Texto
+                    </label>
+                    <input 
+                        ref={ register({ required: 'Selecione um tipo de dados  para a Informação' }) }
+                        className='ml-2 mr-1'
+                        type='radio' 
+                        name='datatype' 
+                        value='image' 
+                        id='image' />
+                    <label 
+                        className='text-gray-800'
+                        htmlFor='image'
+                    >
+                        Imagem
+                    </label>
+                    <input 
+                        ref={ register({ required: 'Selecione um tipo de dados  para a Informação' }) }
+                        className='ml-2 mr-1'
+                        type='radio' 
+                        name='datatype' 
+                        value='video' 
+                        id='video' />
+                    <label 
+                        className='text-gray-800'
+                        htmlFor='video'
+                    >
+                        Vídeo
+                    </label>
+                </div>
+                { errors.datatype && <p className='text-red-700 text-sm px-1'> 
+                    { errors.datatype.message } 
+                </p> }
+                { watchDataType &&
+                    <label 
+                        className='text-sm text-gray-500 ml-1 mt-2'
+                        htmlFor='datatype'>
+                        Valor da Informação 
+                    </label>
+                }
+                { 
+                    watchDataType === 'text' &&
+                    <textarea
+                        className='resize-none shadow p-1 rounded text-md outline-none focus:shadow-outline' 
+                        type='text' 
+                        rows='5'
+                        ref={ register({ 
+                            validate: value => 
+                                (watchDataType === 'text' 
+                                    && value !== undefined 
+                                    && value !== "") || 
+                                'Insira o texto da informação' 
+                        }) }
+                        //defaultValue={ initData?.name }
+                        name='textValue' 
+                    ></textarea>
+                }
+                { errors.textValue && <p className='text-red-700 text-sm px-1'> 
+                    { errors.textValue.message } 
+                </p> }
+                { 
+                    watchDataType === 'image' &&
+                    <input 
+                        className='shadow p-1 rounded text-md outline-none focus:shadow-outline' 
+                        type='url' 
+                        ref={ register({ 
+                            validate: value => 
+                                (watchDataType === 'image' 
+                                    && value !== undefined 
+                                    && value !== "") || 
+                                'Insira a URL da imagem da informação' 
+                        }) }
+                        //defaultValue={ initData?.name }
+                        name='imageValue' 
+                    />
+                }
+                { errors.imageValue && <p className='text-red-700 text-sm px-1'> 
+                    { errors.imageValue.message } 
+                </p> }
+                { 
+                    watchDataType === 'video' &&
+                    <input 
+                        className='shadow p-1 rounded text-md outline-none focus:shadow-outline' 
+                        type='url' 
+                        ref={ register({ 
+                            validate: value => 
+                                (watchDataType === 'video' 
+                                    && value !== undefined 
+                                    && value !== "") || 
+                                'Insira a URL do vídeo da informação' 
+                        }) }
+                        //defaultValue={ initData?.name }
+                        name='videoValue' 
+                    />
+                }
+                { errors.videoValue && <p className='text-red-700 text-sm px-1'> 
+                    { errors.videoValue.message } 
+                </p> }
                 { /* TODO: finalize form data for all types of slides */ }
                 <button 
                     className='bg-green-500 hover:bg-green-600 py-2 mt-8 rounded text-white font-bold shadow focus:outline-none focus:shadow-outline'
